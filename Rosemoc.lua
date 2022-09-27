d64eedcad68fae044df433e4ececf1ddff33f7b9
Ericd71 <105111207+Ericd71@users.noreply.github.com>
2022-09-27 17:57:34 +0200

    Update Rosemoc.lua

--- Rosemoc.lua
+++ Rosemoc.lua
@@ -8,7 +8,7 @@ getgenv().ExploitSpecific = "📜"
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ericd71/Rosemoc/main/Library.lua"))()
 getgenv().api = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ericd71/Rosemoc/main/API.lua"))()
-local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ericd71/Rosemoc/main/BSS API.lua"))()
+local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ericd71/Rosemoc/main/BSS_API.lua"))()
 local httpreq = (syn and syn.request) or http_request or (http and http.request) or request
 
 if not isfolder("kocmoc") then makefolder("kocmoc") end
