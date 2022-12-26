94c9fe25effe7d0dd05b9ff0c698017d98c51781
Ericd71 <105111207+Ericd71@users.noreply.github.com>
2022-12-26 21:59:58 +0100

    Update Rosemoc.lua

--- Rosemoc.lua
+++ Rosemoc.lua
@@ -4151,7 +4151,7 @@ task.spawn(function()
                     local gui = attach:FindFirstChild("Planter Gui")
                     if gui then
                         gui.MaxDistance = 1e5
-                        gui.Size = UDim2.new(100, 0, 25, 0)
+                        gui.Size = UDim2.new(75, 0, 20, 0)
                         
                         local text = gui.Bar.TextLabel
                         if text then
